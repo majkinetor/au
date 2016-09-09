3727dd51c5f6c21093337ba1458faadeb3d4eda7
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-09 22:35:15 +0200

    d

--- vars_default.ps1
+++ vars_default.ps1
@@ -1,3 +1,5 @@
+# Edit variable values and then save this file as 'vars.ps1' to get it included.
+
 $Env:Github_UserRepo   = ''   # Publish to Github; commit git changes
 $Env:Github_ApiKey     = ''   # Publish to Github token
 $Env:NuGet_ApiKey      = ''   # Publish to PSGallery token
