609cd7178c22972cd21c0d0257042aacb1d91856
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-09 15:10:07 +0200

    choco

--- chocolatey/build-package.ps1
+++ chocolatey/build-package.ps1
@@ -1,10 +1,5 @@
 #  Build the chocolatey package based on the latest module build in ..\_build folder
 
-[CmdletBinding()]
-param(
-    [string]$ReleaseNotes = 'https://github.com/majkinetor/au/blob/master/CHANGELOG.md'
-)
-
 $build_path = Resolve-Path $PSScriptRoot\..\_build
 $version    = ls $build_path | sort CreationDate -desc | select -First 1 -Expand Name
 if (![version]$version) { throw 'Latest module build can not be found' }
@@ -12,7 +7,7 @@ if (![version]$version) { throw 'Latest module build can not be found' }
 $module_path = "$build_path\$version\AU"
 $nuspec_path = "$PSScriptRoot\au.nuspec"
 
-Write-Host "==| Building Chocolatey package for AU $version at: '$module_path'`n"
+Write-Host "`n==| Building Chocolatey package for AU $version at: '$module_path'`n"
 
 Write-Host 'Setting description'
 $readme_path = Resolve-Path $PSScriptRoot\..\README.md
@@ -27,11 +22,10 @@ Write-Host 'Updating nuspec file'
 [xml]$au = gc $nuspec_path
 $au.package.metadata.version       = $version.ToString()
 $au.package.metadata.description   = $description
-$au.package.metadata.releaseNotes  = $ReleaseNotes
 $au.Save($nuspec_path)
 
 Write-Host 'Copying module'
 cp -Force -Recurse $module_path $PSScriptRoot\tools
 cp $PSScriptRoot\..\install.ps1 $PSScriptRoot\tools
 
-choco pack
+choco pack $nuspec_path --outputdirectory $PSScriptRoot
