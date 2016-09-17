70431b2362b2ee78ca80c6c0ae426c16c23d55db
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-17 11:48:43 +0200

    runinfo finished

--- AU/Plugins/RunInfo.ps1
+++ AU/Plugins/RunInfo.ps1
@@ -5,16 +5,34 @@ param(
     $Exclude = @('password', 'api_key')
 )
 
-#$i = $Info.Clone()
-#"Saving run info"
-#foreach ($w in $Exclude) {
-    #foreach ($key in $i.Options.Keys) {
-        #if ($key -is [HashTable]) {
-            #foreach ($subkey in $key) {
-                #if ($key -like "*$w*") { $i.Options.$key.$subkey = $null }
-            #}
-        #}
-    #}
-#}
+function deep_clone {
+    param($DeepCopyObject)
 
-$info | Export-CliXML $Path
+    $memStream = new-object IO.MemoryStream
+    $formatter = new-object Runtime.Serialization.Formatters.Binary.BinaryFormatter
+    $formatter.Serialize($memStream,$DeepCopyObject)
+    $memStream.Position=0
+    $formatter.Deserialize($memStream)
+}
+
+$orig_opts = $Info.Options
+$opts      = deep_clone $orig_opts
+$excluded  = ''
+foreach ($w in $Exclude) {
+    foreach ($key in $Info.Options.Keys) {
+        if ($Info.Options.$key -is [HashTable]) {
+            foreach ($subkey in $Info.Options.$key.Keys) {
+                if ($subkey -like "*$w*") {
+                    $excluded += "$key.$subkey "
+                    $opts.$key.$subkey = '******'
+                }
+            }
+        }
+    }
+}
+
+if ($excluded) { Write-Host "  excluded: $excluded" }
+Write-Host "  saving to: $Path"
+$Info.Options = $opts
+$Info | Export-CliXML $Path
+$Info.Options = $orig_opts
