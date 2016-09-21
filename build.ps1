e1bde5ddf33bdf7475f0e0c1526eb91df498ae8e
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-21 22:53:11 +0200

    build.ps1

--- build.ps1
+++ build.ps1
@@ -45,7 +45,7 @@ $b = {
 function zip_module() {
     Write-Host "Creating 7z package"
 
-    $zip_path = "$build_dir\${module_name}_$version.7z"
+    $zip_path = "$build_dir\${module_name}.7z"
     $cmd = "$Env:ChocolateyInstall/tools/7z.exe a '$zip_path' '$module_path' '$installer_path'"
     $cmd | iex | Out-Null
     if (!(Test-Path $zip_path)) { throw "Failed to build 7z package" }
