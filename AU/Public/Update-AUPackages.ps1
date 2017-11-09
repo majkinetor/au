b40596aac522ea586224c378af4f64f07773ad24
Rich Siegel <rismoney@gmail.com>
2017-11-09 13:46:19 -0500

    fix bug with counter not showing up on errs

--- AU/Public/Update-AUPackages.ps1
+++ AU/Public/Update-AUPackages.ps1
@@ -137,7 +137,7 @@ function Update-AUPackages {
                     $message += if (!$pkg.Pushed) { ' but push failed!' } else { ' and pushed'}
                 }
                 if ($pkg.Error) {
-                    $message = "$($pkg.Name) ERROR: "
+                    $message = "[$($p)/$($aup.length)] $($pkg.Name) ERROR: "
                     $message += $pkg.Error.ToString() -split "`n" | % { "`n" + ' '*5 + $_ }
                 }
                 Write-Host '  ' $message
