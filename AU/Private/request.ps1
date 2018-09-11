e8e9a29e77f8130215345812b1942cd55fab0366
Miodrag Milić <miodrag.milic@gmail.com>
2018-09-11 18:25:26 +0200

    fix header usage during request
    Closes #164

--- AU/Private/request.ps1
+++ AU/Private/request.ps1
@@ -2,8 +2,17 @@ function request( [string]$Url, [int]$Timeout, $Options ) {
     if ([string]::IsNullOrWhiteSpace($url)) {throw 'The URL is empty'}
     $request = [System.Net.WebRequest]::Create($Url)
     if ($Timeout)  { $request.Timeout = $Timeout*1000 }
-    if ($Options.Headers) { $Options.Headers.Keys | %{ $request.Headers.add($_, $Options.Headers[$_]) } }
-
+ 
+    $Options.Headers.Keys | % { 
+        if ([System.Net.WebHeaderCollection]::IsRestricted($_)) {
+            $key = $_.Replace('-','')
+            $request.$key = $Options.Headers[$_]
+        }
+        else { 
+            $request.Headers.add($_, $Options.Headers[$_])
+        }
+    }
+    
     $response = $request.GetResponse()
     $response.Close()
     $response
