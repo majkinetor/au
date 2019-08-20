f161eb95aa13a065ff7bf0ea8479870caf4a481a
Jekotia <contact@jekotia.net>
2019-08-20 12:16:32 -0400

    Update vars_default.ps1
    Environment variables to accompany GitLab.ps1 plugin

--- vars_default.ps1
+++ vars_default.ps1
@@ -4,3 +4,8 @@ $Env:Github_UserRepo   = ''   # Publish to Github; commit git changes
 $Env:Github_ApiKey     = ''   # Publish to Github token
 $Env:NuGet_ApiKey      = ''   # Publish to PSGallery token
 $Env:Chocolatey_ApiKey = ''   # Publish to Chocolatey token
+
+$Env:gitlab_user            = ''   # GitLab username to use for the push
+$Env:gitlab_api_key         = ''   # GitLab API key associated with gitlab_user
+$Env:gitlab_push_url        = ''   # GitLab URL to push to. Must be HTTP or HTTPS. e.g. https://jekotia:MyPassword@git.example.org/jekotia/au.git
+$Env:gitlab_commit_strategy = ''   # Same values as the Git plugin; single, atomic, or atomictag
