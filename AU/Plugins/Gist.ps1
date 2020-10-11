7021e9975f5acfa271a4390143d3f99e1d28f6dd
Matthew Prenger <Matthew.Prenger@cerner.com>
2020-10-11 01:00:48 -0500

    Enhance the Gist plugin
    * Add support for GitHub Enterprise installations
    * Add support to configure Gist visibility (public/secret)

--- AU/Plugins/Gist.ps1
+++ AU/Plugins/Gist.ps1
@@ -21,19 +21,25 @@ param(
     [string[]] $Path,
 
     # Gist description
-    [string] $Description = "Update-AUPackages Report #powershell #chocolatey"
+    [string] $Description = "Update-AUPackages Report #powershell #chocolatey",
+
+    # GitHub API base url, overridable for GitHub Enterprise installations
+    [string] $GitHubAPI = "https://api.github.com",
+
+    # If the Gist should be created as public or not, ignored when Id is provided
+    [bool] $PublicGist = $true
 )
 
 # Create gist
 $gist = @{
     description = $Description
-    public      = $true
+    public      = $PublicGist
     files       = @{}
 }
 
-ls $Path | % {
+Get-ChildItem $Path | ForEach-Object {
     $name      = Split-Path $_ -Leaf
-    $content   = gc $_ -Raw
+    $content   = Get-Content $_ -Raw
     $gist.files[$name] = @{content = "$content"}
 }
 
@@ -42,25 +48,21 @@ ls $Path | % {
 #https://github.com/majkinetor/au/issues/142
 [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor [System.Net.SecurityProtocolType]::Tls -bor [System.Net.SecurityProtocolType]::Ssl3
 
-$uri  = 'https://api.github.com/gists'
 $params = @{
     ContentType = 'application/json'
     Method      = if ($Id) { "PATCH" } else { "POST" }
-    Uri         = if ($Id) { "$uri/$Id" } else { $uri }
+    Uri         = if ($Id) { "$GitHubAPI/gists/$Id" } else { "$GitHubAPI/gists" }
     Body        = $gist | ConvertTo-Json
     UseBasicparsing = $true
+    Headers     = @{ 'Accept' = 'application/vnd.github.v3+json' }
 }
-
-#$params | ConvertTo-Json -Depth 100 | Write-Host
 
 if ($ApiKey) {
-    $params.Headers = @{
-        Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($ApiKey))
-    }
+    $params.Headers['Authorization'] = ('Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($ApiKey)))
 }
 
-$res = iwr @params
-
-#https://api.github.com/gists/a700c70b8847b29ebb1c918d47ee4eb1/211bac4dbb707c75445533361ad12b904c593491
-$id = (($res.Content | ConvertFrom-Json).history[0].url -split '/')[-2,-1] -join '/'
-"https://gist.github.com/$id"
+$Response = Invoke-WebRequest @params
+if ($Response.StatusCode -in @(200, 201, 304)) {
+    $JsonResponse = $Response.Content | ConvertFrom-Json
+    Write-Output $JsonResponse.html_url
+}
